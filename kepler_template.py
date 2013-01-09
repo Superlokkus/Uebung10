0ab89fd8a54c03f6b11d74f200df188cd4c3d678
Markus Klemm <markus@markusklemm.net>
2013-01-09 18:19:14 +0100

    Wahre Anomalie funzt

--- kepler_template.py
+++ kepler_template.py
@@ -1,9 +1,15 @@
-#!/usr/bin/python                 # python - Interpreter
-# -*- coding: utf-8 -*-           # Sonderzeichen und deutsche Umlaute
+#!/usr/bin/python
+# encoding=utf-8
+
+"""Übung 10 Numerische Nullstellensuche: Keplerproblem"""
+#Markus Klemm WS12/13 Phy-BA
+
+
 from __future__ import division  # problemlose Ganzzahl-Division
 import scipy as sc               # scipy beinhaltet numpy
 import scipy.optimize as opt     # Funktionen für Nullstellensuche
 from matplotlib import pyplot as plt
+
 """
 Nullstellensuche: Kepler-Problem
 """
@@ -16,14 +22,31 @@ Planet = {"Erde": {"Periode": erdjahr, "numEx": 0.0167, "Halbachse": 1.0},
 
 def KeplerEq(E, eps, t, t0, T):
     """ Kepler-Gleichung """
-    return ...
+    return E - eps*sc.sin(E) - 2*sc.pi*(t - t0)/T
 
 def KeplerEq_prime(E, eps, t, t0, T):
     """ 1. Ableitung der Kepler-Gleichung """
-    return ...
+    return 1 - eps*sc.cos(E)
 
 def ex_Anom(planet, t=0, t0=0):
     """ Exzentrische Anomalie als Nullstelle der Kepler-Gleichung """
-    return opt.newton( ... )
+    return opt.newton(KeplerEq,3, args=(Planet["Erde"]["numEx"],t,t0,Planet["Erde"]["Periode"]))
 
 # Weiter mit Berechnung der wahren Anomalie und des Abstandes von der Sonne:
+
+def W_Anom(E,eps):
+    """Wahre Anomalie"""
+    if (E >= 0 and E <= sc.pi):
+        return sc.arccos*((sc.cos(E) - eps) / (1 - eps*sc.cos(E)))
+        
+    elif (E >= sc.pi and E <= 2*sc.pi):
+        return 2*sc.pi - sc.arccos*((sc.cos(E) - eps) / (1 - eps*sc.cos(E)))
+            
+    else: 
+        return None
+        
+def AbstandSonne(t,a,eps):
+    return a * (1 - eps**2) / (1 + eps*sc.cos(W_Anom()))
+
+
+print ex_Anom(["Erde"])
